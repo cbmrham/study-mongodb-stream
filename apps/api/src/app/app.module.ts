f1ecb8c9a610f6cadc41922bb0a952093ffcf703
cbmrham <ryuto.chiba@cbmrham.com>
2023-07-27 18:22:33 +0900

    feat: app module update

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DataAccessUsersModule } from '@study-mongodb-stream/data-access-users';
-import { EventsModule } from './events/events.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DataAccessUsersModule, EventsModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from '@study-mongodb-stream/data-access-users';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly userService: UserService) {}
   async getData() {
-    const users = this.userService.users({}).then((users) => users);
-    console.log(users);
-    return users;
+    return ['hello', 'world'];
   }
 }
