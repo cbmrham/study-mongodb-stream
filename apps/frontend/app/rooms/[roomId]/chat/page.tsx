855459c4bf39a38e39f247f0eadaa87d11f7595a
cbmrham <ryuto.chiba@cbmrham.com>
2023-08-02 10:37:22 +0900

    refactor: Devide chat component

--- apps/frontend/app/rooms/[roomId]/chat/page.tsx
+++ apps/frontend/app/rooms/[roomId]/chat/page.tsx
@@ -1,55 +1,11 @@
 'use client';
-import { Container, List, TextField, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { useContext, useEffect, useState } from 'react';
 
 import { useParams, useRouter } from 'next/navigation';
 import { UserContext } from '../../../contexts/UserContext';
-import useChat from './useChat';
-import { Room, User } from '@prisma/client/main';
-
-const RoomChat = ({ user, room }: { user: User; room: Room }) => {
-  const [currentUser] = useContext(UserContext);
-  const [postContent, setPostContent] = useState('');
-  const router = useRouter();
-  const params = useParams();
-  const roomId = params?.roomId as string | undefined;
-  const { post, postLogs } = useChat(user.id, room.id);
-  if (!currentUser || !currentUser?.id || !roomId) {
-    router.push('/');
-    return;
-  }
-
-  return (
-    <Container maxWidth="lg" sx={{ mt: '20px' }}>
-      <Typography variant="h2" fontWeight={'bold'}>
-        {room.name}
-      </Typography>
-      <List>
-        {postLogs.map((post) => (
-          <div key={post.id}>
-            <Typography variant="body1" fontWeight={'bold'}>
-              {post.senderId}
-            </Typography>
-            <Typography variant="body2">{post.content}</Typography>
-          </div>
-        ))}
-      </List>
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Message"
-        value={postContent}
-        onChange={(e) => setPostContent(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            postContent && post(postContent);
-            setPostContent('');
-          }
-        }}
-      />
-    </Container>
-  );
-};
+import { Room } from '@prisma/client/main';
+import Chat from './chat';
 
 export default function Rooms() {
   const [currentUser] = useContext(UserContext);
@@ -85,7 +41,7 @@ export default function Rooms() {
         <Typography variant="h2" fontWeight={'bold'}>
           Chat
         </Typography>
-        {currentUser && room && <RoomChat user={currentUser} room={room} />}
+        {currentUser && room && <Chat user={currentUser} room={room} />}
       </Container>
     </main>
   );
